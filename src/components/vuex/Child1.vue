367c6e0d0918b66d4ac68f4edfe25a74064cd1ff
jiangwenhao <qdjiangwenhao@126.com>
2021-09-03 10:47:28 +0800

    echarts 引入及实现 vuex 持久化

--- src/components/vuex/Child1.vue
+++ src/components/vuex/Child1.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>审核单据</h1>
-    <p>实际金额: {{ $store.state.name }}</p>
+    <p>实际金额: {{ $store.state.count }}</p>
+    <p>实时状态: {{ $store.state.status }}</p>
     <button @click="handleChildOneClick">刷新金额</button>
+    <button @click="check">审核金额</button>
   </div>
 </template>
 
@@ -14,6 +16,10 @@ export default {
       // this.myName = '小夏';
       // eventBus.$emit('ChildOneClick', '小夏');
       this.$store.commit('changeName');
+    },
+    check(){
+      //审核单据
+      this.$store.commit('checkStatus')
     }
   }
 }
--- src/components/vuex/Child2.vue
+++ src/components/vuex/Child2.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <h1>发票</h1>
-    <p>发票金额: {{ $store.state.name }}</p>
+    <p>发票金额: {{ $store.state.count }}</p>
+    <p>实时状态: {{ $store.state.status }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style>
-
 </style>
